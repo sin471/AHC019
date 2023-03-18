63c5ce0547fcf64efef11a0f422f9dc913e9a712
sin471 <tachiken.0414.ken@gmail.com>
2023-03-18 20:44:24 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+D = int(input())
+f = [[] for _ in range(2)]
+r = [[] for _ in range(2)]
+
+f_silhouetted = [[[0] * D for _ in range(D)] for _ in range(2)]
+r_silhouetted = [[[0] * D for _ in range(D)] for _ in range(2)]
+
+for i in range(2):
+    for _ in range(D):
+        f[i].append(input())
+    for _ in range(D):
+        r[i].append(input())
+
+b = [[0 for _ in range(D**3)] for _ in range(2)]
+n = 0
+
+for i in range(2):
+    for x in range(D):
+        for y in range(D):
+            for z in range(D):
+                if not (f[i][z][x] == "1" and r[i][z][y] == "1"):
+                    continue
+
+                if f_silhouetted[i][z][x] and r_silhouetted[i][z][y]:
+                    b[i][x * (D**2) + y * D + z] = 0
+                    continue
+
+                n += 1
+                b[i][x * (D**2) + y * D + z] = n
+                f_silhouetted[i][z][x] = 1
+                r_silhouetted[i][z][y] = 1
+
+
+print(n)
+print(" ".join(map(str, b[0])))
+print(" ".join(map(str, b[1])))
