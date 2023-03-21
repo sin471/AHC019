72fd3b5112ab6001f312ef5050a2a709de3fc455
sin471 <tachiken.0414.ken@gmail.com>
2023-03-21 23:02:45 +0900

    共通して埋めれるブロック塊を先に埋めるようにした

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ D, f, r = input_()
 
 f_silhouetted = [[[0] * D for _ in range(D)] for _ in range(2)]
 r_silhouetted = [[[0] * D for _ in range(D)] for _ in range(2)]
+can_filled = [[[[0] * D for _ in range(D)] for _ in range(D)] for _ in range(2)]
+
 b = [[0 for _ in range(D**3)] for _ in range(2)]
 n = 0
 
@@ -26,28 +28,43 @@ for i in range(2):
     for x in range(D):
         for y in range(D):
             for z in range(D):
-                if not (f[i][z][x] == 1 and r[i][z][y] == 1):
-                    continue
+                if f[i][z][x] == 1 and r[i][z][y] == 1:
+                    can_filled[i][x][y][z] = 1
 
+
+# 1組と2組どちらでも共通して埋めれる場所を探す
+for x in range(D):
+    for y in range(D):
+        z = 0
+        while z < D:
+            z2 = z
+            while z2 < D and can_filled[0][x][y][z2] and can_filled[1][x][y][z2]:
+                if z2 == z:
+                    n += 1
+                for i in range(2):
+                    b[i][x * (D**2) + y * D + z2] = n
+                    f_silhouetted[i][z][x] = 1
+                    r_silhouetted[i][z][y] = 1
+                z2 += 1
+            z = z2
+            z += 1
+
+for i in range(2):
+    n2 = n + 1
+    for x in range(D):
+        for y in range(D):
+            for z in range(D):
                 if f_silhouetted[i][z][x] and r_silhouetted[i][z][y]:
                     continue
+                if not can_filled[i][x][y][z]:
+                    continue
+                if b[i][x * (D**2) + y * D + z]:
+                    continue
 
-                n += 1
-                b[i][x * (D**2) + y * D + z] = n
+                b[i][x * (D**2) + y * D + z] = n2
                 f_silhouetted[i][z][x] = 1
                 r_silhouetted[i][z][y] = 1
-
-    # 1組のブロックの中で2組に使い回せるものを使い回す
-    if i == 0:
-        b0_cnt = max(b[0])
-        b1_cnt = 0
-        for j, _ in enumerate(b[0]):
-            x, y, z = j // (D**2), (j % (D**2)) // D, j % D
-            if f[1][z][x] and r[1][z][y] and b1_cnt < b0_cnt:
-                b1_cnt += 1
-                b[1][j] = b1_cnt
-                f_silhouetted[1][z][x] = 1
-                r_silhouetted[1][z][y] = 1
+                n2 += 1
 
 
 def output(n: int, b: List[List[int]]):
@@ -56,4 +73,5 @@ def output(n: int, b: List[List[int]]):
     print(" ".join(map(str, b[1])))
 
 
+n = max(max(b[0]), max(b[1]))
 output(n, b)
