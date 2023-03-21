5cf0ddb0f2c09251e419a60a17c9616ad21584de
sin471 <tachiken.0414.ken@gmail.com>
2023-03-21 23:00:53 +0900

    Add error handling

--- scoring.py
+++ scoring.py
@@ -1,12 +1,25 @@
 import pathlib
-import subprocess
+import subprocess as sub
 import sys
 from typing import List
+
 # Config
 INPUT_DIR_PATH = pathlib.Path(r"b36525d8_windows\tools_x86_64-pc-windows-gnu\in")
 SCORE_PATH = r"b36525d8_windows\tools_x86_64-pc-windows-gnu\vis.exe"
 OUTPUT_FILE_PATH = "output.txt"
 EXECUTE_PATH = "main.py"
+
+def format_score(score_process_output: str):
+    # 出力形式によってここを変更する
+    output = score_process_output.rstrip("\n")
+    try:
+        score = int(output.split()[-1])
+    except ValueError as e:
+        print(f"LocalTestError\n<{output}>")
+        raise e
+
+    return int(score)
+
 
 scores: List[int] = []
 print("Calculating...")
@@ -17,14 +30,14 @@ for i, input_file_path in enumerate(INPUT_DIR_PATH.iterdir(), 1):
     )
     score_command = f"{SCORE_PATH} {input_file_path} {OUTPUT_FILE_PATH}"
     # shell=Trueでは、インジェクション攻撃が可能となるため、内容不明の変数を含めない
-    subprocess.run(
-        execute_command,
+    sub.run(execute_command, shell=True)
+    score_process = sub.run(
+        score_command,
         shell=True,
+        stdout=sub.PIPE,
+        text=True,
     )
-    score_equation = subprocess.run(
-        score_command, shell=True, stdout=subprocess.PIPE, text=True
-    ).stdout.rstrip("\n")
-    score = int(score_equation.split()[-1])
+    score = format_score(score_process.stdout)
     scores.append(score)
     print(f"Case{i} Done")
 
