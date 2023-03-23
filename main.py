13e8967b3c1f19bbb79a00f9bd4c8e8f21e7cd79
sin471 <tachiken.0414.ken@gmail.com>
2023-03-23 14:02:12 +0900

    Add DFS  to fill connected component

--- main.py
+++ main.py
@@ -22,32 +22,46 @@ xyz = [(i, j, k) for i in range(D) for j in range(D) for k in range(D)]
 f_silhouetted = [[[0] * D for _ in range(D)] for _ in range(2)]
 r_silhouetted = [[[0] * D for _ in range(D)] for _ in range(2)]
 can_filled = [[[[0] * D for _ in range(D)] for _ in range(D)] for _ in range(2)]
+is_overlapped = [[[0] * D for _ in range(D)] for _ in range(D)]
+
 b = [[0 for _ in range(D**3)] for _ in range(2)]
 n = 0
 
-for i in range(2):
-    for x, y, z in xyz:
+
+for x, y, z in xyz:
+    for i in range(2):
         if f[i][z][x] == 1 and r[i][z][y] == 1:
             can_filled[i][x][y][z] = 1
 
+    if can_filled[0][x][y][z] and can_filled[1][x][y][z]:
+        is_overlapped[x][y][z] = 1
 
-# 1組と2組どちらでも共通して埋めれる場所を探す
-# 共通ブロック判定の基準軸を変えたときの評価値(低いほどいい)は500ケースでもy<x<zの順だった
-for x in range(D):
-    for z in range(D):
-        y = 0
-        while y < D:
-            y2 = y
-            while y2 < D and can_filled[0][x][y2][z] and can_filled[1][x][y2][z]:
-                if y2 == y:
-                    n += 1
-                for i in range(2):
-                    b[i][x * (D**2) + y2 * D + z] = n
-                    f_silhouetted[i][z][x] = 1
-                    r_silhouetted[i][z][y2] = 1
-                y2 += 1
-            y = y2
-            y += 1
+
+# 共通した連結成分をDFSで埋める
+def fill_connected_component(x: int, y: int, z: int, n: int):
+    global b
+    global is_overlapped
+    global f_silhouetted
+    global r_silhouetted
+    is_inside = all(0 <= i < D for i in [x, y, z])
+    position = x * (D**2) + y * D + z
+    if not (is_inside and is_overlapped[x][y][z]):
+        return
+    b[0][position] = b[1][position] = n
+    is_overlapped[x][y][z] = 0
+    for i in range(2):
+        f_silhouetted[i][z][x] = 1
+        r_silhouetted[i][z][y] = 1
+
+    d = [[1, 0, 0], [0, 1, 0], [0, 0, 1], [-1, 0, 0], [0, -1, 0], [0, 0, -1]]
+    for dx, dy, dz in d:
+        fill_connected_component(x + dx, y + dy, z + dz, n)
+
+
+for x, y, z in xyz:
+    if is_overlapped[x][y][z]:
+        n += 1
+        fill_connected_component(x, y, z, n)
 
 for i in range(2):
     n2 = n + 1
